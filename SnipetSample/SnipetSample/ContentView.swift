c887f0b035539a74dac791f1e274a767b9de287a
Mitti09 <develop.application01@gmail.com>
2024-04-28 12:47:24 +0900

    ✨プロジェクトの追加

---
+++ SnipetSample/SnipetSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnipetSample
+//
+//  Created by DevApp on 2024/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnipetSample/SnipetSample/SnipetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipetSampleApp.swift
+//  SnipetSample
+//
+//  Created by DevApp on 2024/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SnipetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
