fef7e9cef52035bcabd444d9d0b1353f5dbccca4
Mitti09 <develop.application01@gmail.com>
2024-04-28 12:53:58 +0900

    ✨useCaseの雛形を作成

---
+++ SnipetSample/SnipetSample/UseCase.swift
@@ -0,0 +1,24 @@
+//
+//  UseCase.swift
+//  SnipetSample
+//
+//  Created by DevApp on 2024/04/28.
+//
+
+import Foundation
+
+protocol UseCaseInput: AnyObject {
+    
+}
+
+final class UseCase {
+    
+    init() {
+        
+    }
+    
+}
+
+extension UseCase: UseCaseInput {
+    
+}
